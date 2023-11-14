b16799bb93390a10b133110fc7a9bd139507eda2
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-11-14 16:50:15 -0700

    feat: passing all restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,42 @@
+function createRestaurant(name, menus) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+    return restaurant.menus[item.type].push(item)
+  }
+}
+
+function removeMenuItem(restaurant, item, type) {
+ for (i = 0; i < restaurant.menus[type].length; i++) {
+  if (restaurant.menus[type][i].name === item) {
+    restaurant.menus[type].splice(i, 1)
+    return `No one is eating our ${item} - it has been removed from the ${type} menu!`
+    }
+  }
+  return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
+
+function checkForFood(restaurant, item) {
+  for (i = 0; restaurant.menus[item.type].length; i++) {
+    if (restaurant.menus[item.type][i].type === item.type) {
+      return `Yes, we're serving ${item.name} today!`
+    }
+  }
+  return `Sorry, we aren't serving ${item.name} today.`
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem,
+  checkForFood
 }
